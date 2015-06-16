b089d85b2897a69aa8634c04745ea9587e0636b4
Román Gorojovsky <roman@monarlab.com>
2015-06-16 18:11:23 -0300

    Tomo bien el compas

--- parser_rules.py
+++ parser_rules.py
@@ -14,7 +14,7 @@ def p_def_tempo(sub):
 
 def p_def_compas(sub):
     'def_compas : DEF_COMPAS num BARRA num'
-    sub[0] = DefCompas(Numero(sub[1]), sub[3])
+    sub[0] = DefCompas(Numero(sub[2]), sub[4])
 
 def p_constantes(sub):
     '''constantes : constante PUNTO_Y_COMA
